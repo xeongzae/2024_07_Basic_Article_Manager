452ddc9b66385351f59b1dd988d2212e3e8e6ca8
xeongzae <xeongzae@gmail.com>
2024-07-15 20:33:01 +0900

    실제 데이터값으로 남기는 작업 중 좌절

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +10,8 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int lastArticleId = 0;
+		
+		List<Article> articles = new ArrayList<>();
 		
 		while (true) {
 			System.out.printf("명령어) ");
@@ -25,13 +29,14 @@ public class Main {
 			
 			if (cmd.equals("article write")) {
 				System.out.printf("제목 : ");
-				sc.nextLine();
+				String title = sc.nextLine();
 				System.out.printf("내용 : ");
-				sc.nextLine();
+				String body = sc.nextLine();
+				//막상 자신있게 했는데 아무것도 모름
 				
 				System.out.println(++lastArticleId + "번 글이 생성되었습니다");
 			} else if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다");
+				System.out.println("--여기는 수정해야하는데 어케하는지 몰라서 고민중--");
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
@@ -41,4 +46,16 @@ public class Main {
 		
 		System.out.println("== 프로그램 끝 ==");
 	}
+}
+
+class Article{
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body){
+		
+	}
+	
+	
 }
