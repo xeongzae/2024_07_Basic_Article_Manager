a4a9d71ebd9579f95b5f2c44bfde527a402e6c34
xeongzae <xeongzae@gmail.com>
2024-07-15 19:53:13 +0900

    artilce write 진행중

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,44 @@
+
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
+		
 		Scanner sc = new Scanner(System.in);
 		
+		int lastArticleId = 0;
 		
-		// -----연습----
-		// ------
-		
-		while(true) {
+		while (true) {
 			System.out.printf("명령어) ");
 			
-			String cmd = sc.nextLine();
-			System.out.println("입력된 명령어) " + cmd);
+			String cmd = sc.nextLine().trim();
 			
-			if(cmd.equals("exit")) {
+			if (cmd.equals("exit")) {
 				break;
+			} 
+			
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+			
+			if (cmd.equals("article write")) {
+				System.out.printf("제목 : ");
+				sc.nextLine();
+				System.out.printf("내용 : ");
+				sc.nextLine();
+				
+				System.out.println(++lastArticleId + "번 글이 생성되었습니다");
+			} else if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
+		
 		sc.close();
+		
 		System.out.println("== 프로그램 끝 ==");
-	}	
+	}
 }
-
